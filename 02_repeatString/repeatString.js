b5524e4af7b79892a01331084ba47787d49859bf
Sumon C <133718514+SumonGFC@users.noreply.github.com>
2024-01-12 18:46:43 -0500

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0)
+        return "ERROR";
+    let tmp = '';
+    for (let i = 0; i < num; ++i)
+        tmp += string;
+    return tmp;
 };
 
 // Do not edit below this line

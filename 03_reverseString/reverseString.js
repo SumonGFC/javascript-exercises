02645f6a401003adea9862d3a65ec789b6ad69aa
Sumon C <133718514+SumonGFC@users.noreply.github.com>
2024-01-12 19:02:35 -0500

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,20 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
-};
+    if (typeof string !== 'string')
+        return "ERROR";
 
+    let reversed = '';
+    let len = string.length;
+
+    for (let i = 1; i <= len; ++i) {
+        reversed += string[len - i];
+    }
+    return reversed;
+}
+
+
+console.log(reverseString('asd'));
+console.log(reverseString('asd asd'));
+console.log(reverseString(''));
 // Do not edit below this line
 module.exports = reverseString;

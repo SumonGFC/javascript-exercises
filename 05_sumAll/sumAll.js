e244138d413a4b95d40cc4e6d0e4cae09b91c8bf
Sumon C <133718514+SumonGFC@users.noreply.github.com>
2024-01-13 14:27:54 -0500

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    if (!Number.isInteger(x) || !Number.isInteger(y) || x < 0 || y < 0) return "ERROR";
 
+    const start = Math.min(x, y);
+    const end = Math.max(x, y);
+
+    let sum = 0;
+
+    for (let i = start; i <= end; ++i)
+        sum += i
+
+    return sum;
 };
+
+console.log(sumAll(1, 5));  //15
+console.log(sumAll(1, -1)); //ERROR
+console.log(sumAll(0, 0));  //0
+console.log(sumAll('', 5)); //ERROR
+console.log(sumAll(NaN, 5));
 
 // Do not edit below this line
 module.exports = sumAll;

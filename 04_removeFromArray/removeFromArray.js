0c6031d7a1960908171732b08337ddea24470e30
Sumon C <133718514+SumonGFC@users.noreply.github.com>
2024-01-13 12:30:21 -0500

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...items) {
+    //Remove only the first instance of the item
+    for (item of items) {
+        if (arr.includes(item))
+            arr.splice(arr.indexOf(item), 1);
+    }
+    return arr;
+}
 
-};
+console.log(removeFromArray([1,2,3,4], 1, 'q', undefined, 3, '')); 
 
 // Do not edit below this line
 module.exports = removeFromArray;

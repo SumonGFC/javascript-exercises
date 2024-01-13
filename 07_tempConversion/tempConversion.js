0cb7d1faf0d5eed8c55625a835c78fb985063d48
Sumon C <133718514+SumonGFC@users.noreply.github.com>
2024-01-13 15:16:25 -0500

    Complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+    const converted = (tempF - 32)*(5/9);
+    return parseFloat((converted).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+    const converted = (tempC*(9/5) + 32);
+    return parseFloat((converted).toFixed(1));
 };
 
 // Do not edit below this line

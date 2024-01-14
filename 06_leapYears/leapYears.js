f6d83808611d472a4b541404ef1a30435e98f7d4
Sumon C <133718514+SumonGFC@users.noreply.github.com>
2024-01-14 11:19:38 -0500

    Complete exercise 9

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,9 +1,9 @@
 const leapYears = function(year) {
     if (!Number.isInteger(year)) return "ERROR";
 
-    if (year % 4 == 0) {
-        if (year % 100 == 0)
-            return (year % 400 == 0);
+    if (year % 4 === 0) {
+        if (year % 100 === 0)
+            return (year % 400 === 0);
     return true;
     }
     else return false;
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let cleanedString = string.replaceAll(/[^a-zA-Z0-9]+/g, '').toLowerCase();
+    let i = 0;
+    const len = cleanedString.length - 1;
 
+    while (i < len/2) {
+        if (cleanedString[i] !== cleanedString[len - i])
+            return false;
+        ++i;
+    }
+    return true;
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;

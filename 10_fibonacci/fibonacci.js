69e65a9b000e335f07369892db7700fc95d32a25
Sumon C <133718514+SumonGFC@users.noreply.github.com>
2024-02-15 13:48:00 -0500

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let fibs = [0, 1];
+    let nextFib;
+    num = Number(num);
+    if (num < 0 || isNaN(num)) return "OOPS";
+    for (let i = 0; i < num; ++i) {
+        nextFib = fibs.reduce((x, y) => x + y);
+        fibs[0] = fibs[1];
+        fibs[1] = nextFib;
+    }
+    return fibs[0];
 };
 
+console.log(fibonacci(0));
+console.log(fibonacci(1));
+console.log(fibonacci(2));
+console.log(fibonacci(3));
+console.log(fibonacci(4));
+console.log(fibonacci(5));
+console.log(fibonacci(6));
+console.log(fibonacci(7));
 // Do not edit below this line
 module.exports = fibonacci;

621bbf522c7d80b30c61dc517adc047dff27f9b1
Sumon C <133718514+SumonGFC@users.noreply.github.com>
2024-02-15 14:46:49 -0500

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const date = new Date();
+    const currentYear = date.getFullYear();
+    const ages = (people.map(person => {
+        if (person.yearOfDeath) {
+            return person.yearOfDeath - person.yearOfBirth;
+        } else {
+            return currentYear - person.yearOfBirth;
+        }
+    }));
+    const maxAge = ages.reduce((x, y) => Math.max(x, y));
+    const maxAgeInd = ages.indexOf(maxAge);
+    return people[maxAgeInd];
 };
 
 // Do not edit below this line
